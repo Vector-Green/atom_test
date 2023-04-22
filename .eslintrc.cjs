96f8efee38b0e1ebd8716077e7993f42daea5976
Vector <vectorgreencodedream@gmail.com>
2023-04-22 19:52:10 +0300

    rule eslint for pages MultiWordNames

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,12 @@ module.exports = {
     es2021: true,
     node: true
   },
+  overrides: [
+    {
+      files: ['pages/*.vue', 'pages/**/*.vue'],
+      rules: { 'vue/multi-word-component-names': 'off' }
+    }
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
